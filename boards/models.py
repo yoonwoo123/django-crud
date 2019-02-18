46ed3bdacb0242bcf7a350167fcd349e78255318
yoonwoo <lkkjasd@naver.com>
2019-02-18 06:42:49 +0000

    02_Model-Board 클래스 생성

--- boards/models.py
+++ boards/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Board(models.Model):
+    title = models.CharField(max_length=10) # 몇 글자를 최대로할지 인자로 넘겨줘야함 
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True) # 자동으로 추가
+    updated_at = models.DateTimeField(auto_now=True)
